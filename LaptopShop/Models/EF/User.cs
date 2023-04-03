d2bb3cf89d6cc99c7c86cd9192dc0352c93c0fa0
HoangChan27 <huyhoangsaranghae@gmail.com>
2023-04-03 16:50:30 +0700

    Add project files.

---
+++ LaptopShop/Models/EF/User.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace LaptopShop.Models.EF
+{
+    [Table("User")]
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        [Column(TypeName = "nvarchar(50)")]
+        public string UserName { get; set; } // tên đăng nhập của người dùng
+
+        [Required(ErrorMessage = "This field is required")]
+        [Column(TypeName = "nvarchar(50)")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        [Column(TypeName = "nvarchar(50)")]
+        public string FullName { get; set; } // tên đầy đủ của người dùng
+
+        [Column(TypeName = "nvarchar(70)")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        [Column(TypeName = "nvarchar(20)")]
+        public string PhoneNumber { get; set; }
+
+        [Column(TypeName = "nvarchar(250)")]
+        public string? Address { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
